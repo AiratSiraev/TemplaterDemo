3de341dcba398457b6567551a7f4dad2aba2c23d
ЭШ <@SHULGAN-G14>
2024-02-11 16:13:34 +0300

    Add project files.

---
+++ TemplaterLibrary/HtmlTemplater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplaterLibrary
+{
+    public class HtmlTemplater
+    {
+        public string CreateHtml(string template, string jsonData)
+        {
+            return "";
+        }
+    }
+}
